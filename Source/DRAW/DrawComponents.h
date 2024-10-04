1ad1c257e5bb6ba1da8029050065e1cf0b257eba
atifibrahim7 <bigdream382@gmail.com>
2024-10-04 17:35:11 +0500

    Initialize

---
+++ Source/DRAW/DrawComponents.h
@@ -0,0 +1,105 @@
+#ifndef DRAW_COMPONENTS_H
+#define DRAW_COMPONENTS_H
+
+
+#include "./Utility/load_data_oriented.h"
+
+namespace DRAW
+{
+	//*** TAGS ***//
+
+	//*** COMPONENTS ***//
+	struct VulkanRendererInitialization
+	{
+		std::string vertexShaderName;
+		std::string fragmentShaderName;
+		VkClearColorValue clearColor;
+		VkClearDepthStencilValue depthStencil;
+		float fovDegrees;
+		float nearPlane;
+		float farPlane;
+	};
+
+	struct VulkanRenderer
+	{
+		GW::GRAPHICS::GVulkanSurface vlkSurface;
+		VkDevice device = nullptr;
+		VkPhysicalDevice physicalDevice = nullptr;
+		VkRenderPass renderPass;
+		VkShaderModule vertexShader = nullptr;
+		VkShaderModule fragmentShader = nullptr;
+		VkPipeline pipeline = nullptr;
+		VkPipelineLayout pipelineLayout = nullptr;
+		GW::MATH::GMATRIXF projMatrix;
+		VkDescriptorSetLayout descriptorLayout = nullptr;
+		VkDescriptorPool descriptorPool = nullptr;
+		std::vector<VkDescriptorSet> descriptorSets;
+		VkClearValue clrAndDepth[2];
+	};
+
+	struct VulkanVertexBuffer
+	{
+		VkBuffer buffer = VK_NULL_HANDLE;
+		VkDeviceMemory memory = VK_NULL_HANDLE;
+	};
+
+	struct VulkanIndexBuffer
+	{
+		VkBuffer buffer = VK_NULL_HANDLE;
+		VkDeviceMemory memory = VK_NULL_HANDLE;
+	};
+
+	struct GeometryData
+	{
+		unsigned int indexStart, indexCount, vertexStart;
+		inline bool operator < (const GeometryData a) const {
+			return indexStart < a.indexStart;
+		}
+	};
+	
+	struct GPUInstance
+	{
+		GW::MATH::GMATRIXF	transform;
+		H2B::ATTRIBUTES		matData;
+	};
+
+	struct VulkanGPUInstanceBuffer
+	{
+		unsigned long long element_count = 1;
+		std::vector<VkBuffer> buffer;
+		std::vector<VkDeviceMemory> memory;
+	};
+
+	struct SceneData
+	{
+		GW::MATH::GVECTORF sunDirection, sunColor, sunAmbient, camPos;
+		GW::MATH::GMATRIXF viewMatrix, projectionMatrix;
+	};
+
+	struct VulkanUniformBuffer
+	{
+		std::vector<VkBuffer> buffer;
+		std::vector<VkDeviceMemory> memory;
+	};
+
+
+	struct Camera
+	{
+		GW::MATH::GMATRIXF camMatrix;
+	};	
+
+	struct CPULevel
+	{
+		std::string jsonPath;       // Path to the JSON file
+		std::string modelFolderPath; // Path to the folder with model assets
+		Level_Data levelData;       // Structure to hold the loaded level data
+	};
+
+	// Component to represent level data on the GPU
+	struct GPULevel
+	{
+		// This component doesn't need to store anything
+	};
+
+} // namespace DRAW
+#endif // !DRAW_COMPONENTS_H
